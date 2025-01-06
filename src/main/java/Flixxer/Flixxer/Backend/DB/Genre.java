637d9fc5695025a26e4aeb80c3cd38efa63a8cbe
sgreloch <samuelgreloch@yahoo.com>
2025-01-06 15:28:17 -0500

    app

--- src/main/java/Flixxer/Flixxer/Backend/DB/Genre.java
+++ src/main/java/Flixxer/Flixxer/Backend/DB/Genre.java
@@ -1,14 +1,33 @@
 package Flixxer.Flixxer.Backend.DB;
 
 import jakarta.persistence.*;
+import java.time.LocalDate;
 
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "genre")
 public class Genre {
+
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Getter
+    @Setter
+    @Column
+    private String title;
+
+    @Getter
+    @Setter
+    @Column
+
+    private String genre;
+
+
+
 
     public void setId(Long id) {
         this.id = id;
