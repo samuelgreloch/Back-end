d421650bf4d4835ffc2a46626c8ec3b53e776dcd
sgreloch <samuelgreloch@yahoo.com>
2025-01-06 10:46:05 -0500

    Enviroments

---
+++ src/main/java/Flixxer/Flixxer/Backend/Controller/VideoController.java
@@ -0,0 +1,53 @@
+package Flixxer.Flixxer.Backend.Controller;
+import Flixxer.Flixxer.Backend.DB.Video;
+import Flixxer.Flixxer.Backend.VideoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
+
+@RestController
+public class VideoController {
+
+    @GetMapping(value = "/w")
+    public String getPage(){
+        return "Hello World";
+    }
+
+
+    @Autowired
+    private VideoRepository videoRepository;
+
+    @GetMapping(value = "/videos")
+    public List<Video> getVideos() {
+        return videoRepository.findAll();
+    }
+
+    @PostMapping(value ="/save")
+    public String saveVideo(@RequestBody Video video) {
+        videoRepository.save(video);
+        return "Video saved";
+    }
+
+    @PutMapping(value = "update/{content_id}")
+    public String updateVideo(@PathVariable long content_id, @RequestBody Video video) {
+        Video updatedVideo = videoRepository.findById(content_id).get();
+        updatedVideo.setDescription(video.getDescription());
+        updatedVideo.setTitle(video.getTitle());
+        updatedVideo.setGenre(video.getGenre());
+        updatedVideo.setDuration(video.getDuration());
+        updatedVideo.setRating(video.getRating());
+        updatedVideo.setReleaseDate(video.getReleaseDate());
+        videoRepository.save(updatedVideo);
+        return "Video updated";
+    }
+
+    @DeleteMapping(value = "/delete/{content_id}" )
+    public String deleteVideo(@PathVariable long content_id) {
+        Video deleteVideo = videoRepository.findById(content_id).get();
+        videoRepository.delete(deleteVideo);
+        return "Video deleted";
+    }
+
+}
