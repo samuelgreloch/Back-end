bcc6d27bd6ee6c65fa1cb958ea686e6bf09eca52
sgreloch <samuelgreloch@yahoo.com>
2025-01-06 12:47:58 -0500

    Revert "added genere"
    This reverts commit 9f54c1d70c5dfd2f652ca35e402df2c0c824cd6b.

--- src/main/java/Flixxer/Flixxer/Backend/Genre.java
+++
@@ -1,9 +0,0 @@
-package Flixxer.Flixxer.Backend;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.web.bind.annotation.RestController;
-
-
-@RestController
-public interface Genre extends JpaRepository<Genre, Long> {
-}
